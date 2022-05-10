13aedede9e0c4e918cd2814741232c7a2ba2bffe
thanhtuyenle <ttle@mum.edu>
2022-05-09 22:46:14 -0500

    added PUT/PATCH/PUSH & updated GET

---
+++ src/test/java/model/PostBody.java
@@ -0,0 +1,61 @@
+package model;
+
+public class PostBody {
+    private int userId;
+    private int id;
+    private String title;
+    private String body;
+
+    public PostBody(int userId, int id, String title, String body) {
+        this.userId = userId;
+        this.id = id;
+        this.title = title;
+        this.body = body;
+    }
+
+    public PostBody (){
+
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    @Override
+    public String toString() {
+        return "PostBody{" +
+                "userId=" + userId +
+                ", id=" + id +
+                ", title='" + title + '\'' +
+                ", body='" + body + '\'' +
+                '}';
+    }
+}
